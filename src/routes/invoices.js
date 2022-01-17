b6ba6649f40499f2c1a82a7218358b04f36d3126
Hassan Abdinur <hassan.abdinur123@gmail.com>
2022-01-17 00:46:20 -0800

    added filter on the invoices nav list

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -7,6 +7,7 @@ import { getInvoices } from "../data";
 
 const Invoices = () => {
     let invoices = getInvoices();
+    let [searchParams, setSearchParams] = useSearchParams();
     return (
         <div style={{ display: "flex" }}>
             <nav 
@@ -15,21 +16,39 @@ const Invoices = () => {
                     padding: "1rem"
                 }}
             >
-                {invoices.map(invoice => (
-                    <NavLink
-                        style={({ isActive }) => {
-                            return {
-                                display: "block",
-                                margin: "1rem 0",
-                                color: isActive ? "red" : ""
-                            };
-                        }}
-                        to={`/invoices/${invoice.number}`}
-                        key={invoices.number}
-                    >
-                        {invoice.name}
-                    </NavLink>
-                ))}
+                <input 
+                    value={searchParams.get("filter") || ""}
+                    onChange={event => {
+                        let filter = event.target.value;
+                        if (filter) {
+                            setSearchParams({ filter });
+                        } else {
+                            setSearchParams({});
+                        }
+                    }}
+                />
+                    {invoices
+                        .filter(invoice => {
+                            let filter = searchParams.get("filter");
+                            if (!filter) return true;
+                            let name = invoice.name.toLocaleLowerCase();
+                            return name.startsWith(filter.toLowerCase());
+                        })
+                    .map(invoice => (
+                        <NavLink
+                            style={({ isActive }) => {
+                                return {
+                                    display: "block",
+                                    margin: "1rem 0",
+                                    color: isActive ? "red" : ""
+                                };
+                            }}
+                            to={`/invoices/${invoice.number}`}
+                            key={invoices.number}
+                        >
+                            {invoice.name}
+                        </NavLink>
+                    ))}
             </nav>
             <Outlet />
         </div>
