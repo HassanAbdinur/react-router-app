7786cbf132038c99610d1548891b96b58005e0e4
Hassan Abdinur <hassan.abdinur123@gmail.com>
2022-01-16 22:49:12 -0800

    importing expenses

---
+++ src/routes/expenses.js
@@ -0,0 +1,13 @@
+import './Expenses.css'
+
+function Expenses() {
+    return (
+        <main>
+            <h2>
+                Expenses
+            </h2>
+        </main>
+    );
+}
+
+export default Expenses;
---
+++ src/routes/invoices.js
@@ -0,0 +1,13 @@
+import './Invoices.css';
+
+function Invoices() {
+    return (
+        <main>
+            <h2>
+                Invoices
+            </h2>
+        </main>
+    );
+}
+
+export default Invoices;
